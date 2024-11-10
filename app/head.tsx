7d721f07e5e045718ef9a62f98249569e4592f7d
FiveMachine <141573032+FiveMachine@users.noreply.github.com>
2024-11-09 21:54:34 -0500

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -1,12 +1,12 @@
 export default function Head() {
   return (
-    <>
+    <div>
       <title>Roam Around</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Build your custom travel itenerary powered by GPT3" />
       <link rel="icon" href="/favicon.ico" />
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7265051125086713"
      crossOrigin="anonymous"></script>
-    </>
+    </div>
   )
 }
